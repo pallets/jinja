d9599c8904bc9dfde7a782d3f69657ebb0839366
Armin Ronacher <armin.ronacher@active-4.com>
2010-02-10 02:19:17 +0100

    Untagged and added python 3 category.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Markup :: HTML'
