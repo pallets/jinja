9512d3274fe5172ec6e9c6283dc238d7b753f967
Armin Ronacher <armin.ronacher@active-4.com>
2010-05-29 22:54:06 +0200

    Tip is 2.5, not 2.4.2
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ if sys.version_info >= (3, 0):
 
 setup(
     name='Jinja2',
-    version='2.4.2',
+    version='2.5',
     url='http://jinja.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
