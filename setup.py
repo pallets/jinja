04bdf45ffb09a0e61d80a3a9ddbe69c57eeb934b
Matt Robenolt <matt@ydekproductions.com>
2012-05-22 23:44:52 -0700

    Adding a CLI for Jinja2
    Allows compiling templates via the command line by taking data input
    from either json, yaml, ini, or a querystring source.
    
    Data can be read in from a file or piped in through stdin.
    
    See __doc__ for examples on usage.
    
    I am 100% open to critiques or adjustments that need to be made to
    comply with library conformity. :)

--- setup.py
+++ setup.py
@@ -101,10 +101,14 @@ setup(
     extras_require={'i18n': ['Babel>=0.8']},
     test_suite='jinja2.testsuite.suite',
     include_package_data=True,
-    entry_points="""
-    [babel.extractors]
-    jinja2 = jinja2.ext:babel_extract[i18n]
-    """,
+    entry_points={
+        'babel.extractors': [
+            'jinja2 = jinja2.ext:babel_extract[i18n]',
+        ],
+        'console_scripts': [
+            'jinja2 = jinja2.cli:main'
+        ],
+    },
     features={'debugsupport': debugsupport},
     **extra
 )
