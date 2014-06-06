762c612e7276889aac265645da00e62e33d1573c
Armin Ronacher <armin.ronacher@active-4.com>
2014-06-06 22:49:12 +0600

    Bump version number to 2.7.3

--- jinja2/__init__.py
+++ jinja2/__init__.py
@@ -27,7 +27,7 @@
     :license: BSD, see LICENSE for more details.
 """
 __docformat__ = 'restructuredtext en'
-__version__ = '2.7.3-dev'
+__version__ = '2.7.3'
 
 # high level interface
 from jinja2.environment import Environment, Template
