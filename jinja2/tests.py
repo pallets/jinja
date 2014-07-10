1dffd1e2be40fa55c7bebb27307a7fe11e98e38d
Tim Bray <tim@kooky.org>
2014-07-10 11:43:53 +0100

    decimal is a number

--- jinja2/tests.py
+++ jinja2/tests.py
@@ -12,6 +12,7 @@ import re
 from collections import Mapping
 from jinja2.runtime import Undefined
 from jinja2._compat import text_type, string_types, integer_types
+import decimal
 
 
 number_re = re.compile(r'^-?\d+(\.\d+)?$')
@@ -88,7 +89,7 @@ def test_mapping(value):
 
 def test_number(value):
     """Return true if the variable is a number."""
-    return isinstance(value, integer_types + (float, complex))
+    return isinstance(value, integer_types + (float, complex, decimal.Decimal))
 
 
 def test_sequence(value):
