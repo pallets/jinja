56e187c3bbe1fbba79e4bef7e9eb06722371c9d6
Armin Ronacher <armin.ronacher@active-4.com>
2015-07-26 19:50:27 +0200

    This is 2.9.dev

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ from setuptools import setup
 
 setup(
     name='Jinja2',
-    version='2.8',
+    version='2.9.dev',
     url='http://jinja.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
