04b30cdd5c97c35825e8a5b1727382d8569d8347
Adrian Moennich <adrian@planetcoding.net>
2017-01-09 00:21:45 +0100

    Remove unused/duplicate imports

--- jinja2/lexer.py
+++ jinja2/lexer.py
@@ -20,8 +20,7 @@ from operator import itemgetter
 from collections import deque
 from jinja2.exceptions import TemplateSyntaxError
 from jinja2.utils import LRUCache
-from jinja2._compat import iteritems, implements_iterator, text_type, \
-     intern, PY2
+from jinja2._compat import iteritems, implements_iterator, text_type, intern
 
 
 # cache for the lexers. Exists in order to be able to have multiple
