e1e1e7d18b0badcd982b0bbde4ed1ee8d8c19eb3
David Lord <davidism@gmail.com>
2020-01-09 10:38:52 -0800

    move to src directory

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 with io.open("README.rst", "rt", encoding="utf8") as f:
     readme = f.read()
 
-with io.open("jinja2/__init__.py", "rt", encoding="utf8") as f:
+with io.open("src/jinja2/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read(), re.M).group(1)
 
 setup(
@@ -45,9 +45,10 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Markup :: HTML",
     ],
-    packages=find_packages(),
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     include_package_data=True,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=["MarkupSafe>=0.23"],
     extras_require={"i18n": ["Babel>=0.8"]},
     entry_points={"babel.extractors": ["jinja2 = jinja2.ext:babel_extract[i18n]"]},
