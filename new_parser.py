ff7b3202fdbb2fdd142ca43cffed6862598b1cb0
Kevin <kevin@kevin-brown.com>
2020-05-10 10:29:48 -0400

    Start working on new AST parser
    This parser will take the Tatsu-generated AST and generate a compatible
    Jinja AST from it. This should allow us to refine the grammar to
    generate a better AST and also verify that it is producing comparable
    Jinja ASTs that can be used by the current compiler.

---
+++ new_parser.py
@@ -0,0 +1,127 @@
+from jinja2 import nodes
+
+
+def lineno_from_parseinfo(parseinfo):
+    return parseinfo.line + 1
+
+def parse(ast):
+    if isinstance(ast, list):
+        return [parse(item) for item in ast]
+
+    if isinstance(ast, str):
+        return parse_output(ast)
+
+    if 'type' in ast and ast['type'] == 'variable':
+        return parse_print(ast)
+
+    if 'block' in ast:
+        return parse_block(ast)
+
+    if 'start' in ast and 'end' in ast:
+        return parse_block_pair(ast)
+
+    return None
+
+def parse_block(ast):
+    if ast['block']['name'] == 'from':
+        return parse_block_from(ast)
+
+    return None
+
+def parse_block_pair(ast):
+    if ast['start']['name'] == 'with':
+        return parse_block_with(ast)
+
+    return None
+
+def parse_block_from(ast):
+    names = []
+    parameters = ast['block']['parameters']
+
+    if len(parameters) > 2:
+        names = []#parameters[2:]
+
+    from_import = nodes.FromImport(
+        lineno=lineno_from_parseinfo(ast['parseinfo'])
+    )
+    from_import.template = parse_variable(parameters[0]['value'])
+    from_import.names = names
+
+    return from_import
+
+def parse_block_with(ast):
+    with_node = nodes.With(
+        lineno=lineno_from_parseinfo(ast['parseinfo'])
+    )
+
+    targets = []
+    values = []
+
+    for parameter in ast['start']['parameters']:
+        if 'key' not in parameter:
+            raise
+
+        targets.append(parameter['key'])
+        values.append(parameter['value'])
+
+    with_node.targets = targets
+    with_node.values = values
+
+    return with_node
+
+def parse_literal(ast):
+    if 'literal_type' not in ast:
+        raise
+
+    literal_type = ast['literal_type']
+
+    if literal_type == 'string':
+        return nodes.Const(
+            ''.join(ast['value']),
+            lineno=lineno_from_parseinfo(ast['parseinfo'])
+        )
+
+def parse_output(ast):
+    return nodes.Output(
+        [nodes.TemplateData(ast)]
+    )
+
+def parse_print(ast):
+    variable = ast['name']
+
+    node = parse_variable(variable)
+
+    for accessor_ast in variable['accessors']:
+        node = parse_variable_accessor(node, accessor_ast)
+
+    return nodes.Output(node)
+
+def parse_template(ast):
+    return nodes.Template(parse(ast), lineno=1)
+
+def parse_variable(ast):
+    ast = ast['variable']
+
+    if 'literal_type' in ast:
+        return parse_literal(ast)
+
+    return nodes.Name(
+        ast,
+        'load'
+    )
+
+def parse_variable_accessor(node, ast):
+    accessor_type = ast['accessor_type']
+
+    if accessor_type == 'brackets':
+        accessor_node = nodes.Getitem()
+        accessor_node.arg = parse_variable(ast['parameter'])
+    elif accessor_type == 'dot':
+        accessor_node = nodes.Getattr()
+        accessor_node.attr = ast['parameter']['variable']
+
+    accessor_node.node = node
+    accessor_node.ctx = "load"
+    accessor_node.lineno = lineno_from_parseinfo(ast['parseinfo'])
+
+    return accessor_node
--- test_tatsu.py
+++ test_tatsu.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 from tatsu.util import asjson
 import json
+import pprint
 import tatsu
 import sys
+from new_parser import parse_template
+from jinja2.environment import Environment
 
 
 with open('grammar.ebnf', 'r') as tatsu_grammar:
     with open('test_template.jinja', 'r') as test_template:
+        template_string = test_template.read()
+
         grammar_start = datetime.now()
 
         grammar = tatsu.compile(tatsu_grammar.read())
@@ -15,11 +20,34 @@ with open('grammar.ebnf', 'r') as tatsu_grammar:
 
         parse_start = datetime.now()
 
-        ast = grammar.parse(test_template.read(), whitespace='')
+        ast = grammar.parse(template_string, whitespace='', parseinfo=True)
 
         parse_end = datetime.now()
 
-        print(json.dumps(asjson(ast), indent=2))
+        with open('tatsu_jinja.json', 'w') as tatsu_ast_file:
+            json.dump(asjson(ast), tatsu_ast_file, indent=2)
+
+        new_parse_start = datetime.now()
+
+        new_ast = parse_template(ast)
+
+        new_parse_end = datetime.now()
+
+        with open('tatsu_jinja.py', 'w') as new_ast_file:
+            pprint.pprint(new_ast, indent=2, stream=new_ast_file)
+
+        env = Environment()
+
+        jinja_parse_start = datetime.now()
+
+        jinja_ast = env.parse(template_string)
+
+        jinja_parse_end = datetime.now()
+
+        with open('parsed_jinja.py', 'w') as jinja_ast_file:
+            pprint.pprint(jinja_ast, indent=2, stream=jinja_ast_file)
 
         print("Grammar", grammar_end - grammar_start, file=sys.stderr)
         print("Parser", parse_end - parse_start, file=sys.stderr)
+        print("New Parser", new_parse_end - new_parse_start, file=sys.stderr)
+        print("Jinja Parser", jinja_parse_end - jinja_parse_start, file=sys.stderr)
