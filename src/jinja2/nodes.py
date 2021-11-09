pallets/jinja

commit 60293416db69782fd048a7820667afa4ae7c423b

remove more compat code

src/jinja2/nodes.py
@@ -212,15 +212,13 @@ class Node(metaclass=NodeType):
         return self
 
     def __eq__(self, other):
-        return type(self) is type(other) and tuple(self.iter_fields()) == tuple(
-            other.iter_fields()
-        )
+        if type(self) is not type(other):
+            return NotImplemented
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+        return tuple(self.iter_fields()) == tuple(other.iter_fields())
 
-    # Restore Python 2 hashing behavior on Python 3
-    __hash__ = object.__hash__
+    def __hash__(self):
+        return hash(tuple(self.iter_fields()))
 
     def __repr__(self):
         return "%s(%s)" % (
@@ -649,11 +647,6 @@ class Filter(Expr):
         if eval_ctx.volatile or self.node is None:
             raise Impossible()
 
-        # we have to be careful here because we call filter_ below.
-        # if this variable would be called filter, 2to3 would wrap the
-        # call in a list because it is assuming we are talking about the
-        # builtin filter function here which no longer returns a list in
-        # python 3.  because of that, do not rename filter_ to filter!
         filter_ = self.environment.filters.get(self.name)
 
         if filter_ is None or getattr(filter_, "contextfilter", False) is True:

src/jinja2/asyncfilters.py
@@ -146,8 +146,7 @@ ASYNC_FILTERS = {
     "groupby": do_groupby,
     "join": do_join,
     "list": do_list,
-    # we intentionally do not support do_last because that would be
-    # ridiculous
+    # we intentionally do not support do_last because it may not be safe in async
     "reject": do_reject,
     "rejectattr": do_rejectattr,
     "map": do_map,

src/jinja2/asyncsupport.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-"""The code for async support. Importing this patches Jinja on supported
-Python versions.
-"""
+"""The code for async support. Importing this patches Jinja."""
 import asyncio
 import inspect
 from functools import update_wrapper

src/jinja2/compiler.py
@@ -681,6 +681,7 @@ class CodeGenerator(NodeVisitor):
 
         from .runtime import exported
 
+        self.writeline("from __future__ import generator_stop")  # Python < 3.7
         self.writeline("from jinja2.runtime import " + ", ".join(exported))
 
         if self.environment.is_async:

src/jinja2/debug.py
@@ -23,14 +23,9 @@ def rewrite_traceback_stack(source=None):
     if isinstance(exc_value, TemplateSyntaxError) and not exc_value.translated:
         exc_value.translated = True
         exc_value.source = source
-
-        try:
-            # Remove the old traceback on Python 3, otherwise the frames
-            # from the compiler still show up.
-            exc_value.with_traceback(None)
-        except AttributeError:
-            pass
-
+        # Remove the old traceback, otherwise the frames from the
+        # compiler still show up.
+        exc_value.with_traceback(None)
         # Outside of runtime, so the frame isn't executing template
         # code, but it still needs to point at the template.
         tb = fake_traceback(
@@ -121,7 +116,7 @@ def fake_traceback(exc_value, tb, filename, lineno):
         for attr in (
             "argcount",
             "posonlyargcount",  # Python 3.8
-            "kwonlyargcount",  # Python 3
+            "kwonlyargcount",
             "nlocals",
             "stacksize",
             "flags",

src/jinja2/environment.py
@@ -247,9 +247,8 @@ class Environment(object):
             See :ref:`bytecode-cache` for more information.
 
         `enable_async`
-            If set to true this enables async template execution which allows
-            you to take advantage of newer Python features.  This requires
-            Python 3.6 or later.
+            If set to true this enables async template execution which
+            allows using async functions and generators.
     """
 
     #: if this environment is sandboxed.  Modifying this variable won't make
@@ -470,12 +469,11 @@ class Environment(object):
     def call_filter(
         self, name, value, args=None, kwargs=None, context=None, eval_ctx=None
     ):
-        """Invokes a filter on a value the same way the compiler does it.
+        """Invokes a filter on a value the same way the compiler does.
 
-        Note that on Python 3 this might return a coroutine in case the
-        filter is running from an environment in async mode and the filter
-        supports async execution.  It's your responsibility to await this
-        if needed.
+        This might return a coroutine if the filter is running from an
+        environment in async mode and the filter supports async
+        execution. It's your responsibility to await this if needed.
 
         .. versionadded:: 2.7
         """

src/jinja2/filters.py
@@ -564,13 +564,9 @@ def do_filesizeformat(value, binary=False):
         return "%.1f %s" % ((base * bytes / unit), prefix)
 
 
-def do_pprint(value, verbose=False):
-    """Pretty print a variable. Useful for debugging.
-
-    With Jinja 1.2 onwards you can pass it a parameter.  If this parameter
-    is truthy the output will be more verbose (this requires `pretty`)
-    """
-    return pformat(value, verbose=verbose)
+def do_pprint(value):
+    """Pretty print a variable. Useful for debugging."""
+    return pformat(value)
 
 
 @evalcontextfilter
@@ -595,8 +591,8 @@ def do_urlize(
 
        {{ mytext|urlize(40, target='_blank') }}
 
-    .. versionchanged:: 2.8+
-       The *target* parameter was added.
+    .. versionchanged:: 2.8
+       The ``target`` parameter was added.
     """
     policies = eval_ctx.environment.policies
     rel = set((rel or "").split() or [])

src/jinja2/lexer.py
@@ -10,6 +10,7 @@ from collections import deque
 from operator import itemgetter
 from sys import intern
 
+from ._identifier import pattern as name_re
 from .exceptions import TemplateSyntaxError
 from .utils import LRUCache
 
@@ -37,20 +38,6 @@ float_re = re.compile(
     """,
     re.IGNORECASE | re.VERBOSE,
 )
-
-try:
-    # check if this Python supports Unicode identifiers
-    compile("föö", "<unknown>", "eval")
-except SyntaxError:
-    # Python 2, no Unicode support, use ASCII identifiers
-    name_re = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*")
-    check_ident = False
-else:
-    # Unicode support, import generated re pattern and set flag to use
-    # str.isidentifier to validate during lexing.
-    from ._identifier import pattern as name_re
-
-    check_ident = True
 
 # internal the tokens and keep references to them
 TOKEN_ADD = intern("add")
@@ -632,7 +619,7 @@ class Lexer(object):
                 token = value
             elif token == TOKEN_NAME:
                 value = str(value)
-                if check_ident and not value.isidentifier():
+                if not value.isidentifier():
                     raise TemplateSyntaxError(
                         "Invalid character in identifier", lineno, name, filename
                     )

commit 9b96b4817a61d7f114de088cfe729a4625c820bc

Use object.__hash__ for Node.__hash__
This fixes a regression in commit 60293416db69782fd048a7820667afa4ae7c423b that
changed the `__hash__` implementation of Node from the default pointer
hash, to a hash based on the node fields.

Since these fields contains list objects, they are not hashable, making
every call to `Node.__hash__` fail.

This breaks some third-party usage such as in `django-compressor`
(See: https://github.com/django-compressor/django-compressor/issues/1060)

This changed reverts the hash method back to using `object.__hash__` as
the hash implementation.

src/jinja2/nodes.py
@@ -241,8 +241,7 @@ class Node(metaclass=NodeType):
 
         return tuple(self.iter_fields()) == tuple(other.iter_fields())
 
-    def __hash__(self) -> int:
-        return hash(tuple(self.iter_fields()))
+    __hash__ = object.__hash__
 
     def __repr__(self) -> str:
         args_str = ", ".join(f"{a}={getattr(self, a, None)!r}" for a in self.fields)

tests/test_nodes.py
@@ -1,0 +1,3 @@
+def test_template_hash(env):
+    template = env.parse("hash test")
+    hash(template)