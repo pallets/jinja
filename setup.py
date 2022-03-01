e77896c1e9e6ba4a45a8bcd4ad3235f928f89b6b
abishek-rz <52658361+abishek-rz@users.noreply.github.com>
2022-03-01 23:32:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Jinja2",
-    install_requires=["MarkupSafe>=2.0"],
+    install_requires=["MarkupSafe==2.0.1"],
     extras_require={"i18n": ["Babel>=2.7"]},
 )
